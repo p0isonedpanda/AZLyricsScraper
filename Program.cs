51a0aab7bd4a6096ee951d95fd5be1799a344f22
Daniel Coady <daniel.coady@outlook.com.au>
2018-10-17 15:44:30 +1100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml;
+using System.Net;
+using HtmlAgilityPack;
+
+namespace HTMLAgilityTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string html;
+            using (WebClient client = new WebClient())
+            {
+                html = client.DownloadString("https://www.azlyrics.com/lyrics/handslikehouses/afireonahill.html");
+            }
+
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+    
+            HtmlNodeCollection htmlBody = htmlDoc.DocumentNode.SelectNodes("//comment()/..");
+		    
+		    Console.WriteLine(htmlBody[8].InnerText);		
+        }
+    }
+}
